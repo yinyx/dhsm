43f6700e8deb48808c6c29e72bb046ae5d3af697
zhangheng00069952 <z.heng.987654@163.com>
2020-07-15 11:06:46 +0800

    增加故障告警模块

---
+++ src/main/java/com/nari/dhsm/controller/AlarmController.java
@@ -0,0 +1,87 @@
+package com.nari.dhsm.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.nari.dhsm.entity.ErrorCodeEnum;
+import com.nari.dhsm.service.AlarmService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.*;
+
+/**
+ * @author zhangheng
+ * @version 1.0 2020/7/13
+ */
+@RestController
+@RequestMapping(value="/alarm")
+@Api(value="/alarm",tags = {"报警模块接口"},description = "报警模块接口Api")
+public class AlarmController {
+    private Logger log = LoggerFactory.getLogger(AlarmController.class);
+
+    @Autowired
+    private AlarmService alarmService;
+
+    @RequestMapping(value="/getAlarmList",method= RequestMethod.POST)
+    @ApiOperation("获取报警列表信息")
+    public JSONObject getAlarmList(@RequestParam Map<String,Object> map){
+        log.info("==== 获取报警列表信息 ====");
+        JSONObject jsonObject = new JSONObject();
+        try{
+            List<HashMap<String,Object>> alarmList = alarmService.getAlarmList((String) map.get("deviceId"));
+            int alarmNum = alarmService.getAlarmNum((String) map.get("deviceId"));
+                jsonObject.put("list",alarmList);
+                jsonObject.put("num",alarmNum);
+                jsonObject.put("code", ErrorCodeEnum.E00_0001.getCode());
+                jsonObject.put("message",ErrorCodeEnum.E00_0001.getMessage());
+            return jsonObject;
+        }catch (Exception e){
+            jsonObject.put("code", ErrorCodeEnum.E00_0002.getCode());
+            jsonObject.put("message", ErrorCodeEnum.E00_0002.getMessage());
+            return jsonObject;
+        }
+    }
+
+    @RequestMapping(value="/setAlarmStatus",method= RequestMethod.POST)
+    @ApiOperation("设置报警为已处理")
+    public JSONObject setAlarmStatus(@RequestParam Map<String,Object> map){
+        log.info("==== 设置报警为已处理 ====");
+        JSONObject jsonObject = new JSONObject();
+        try{
+            alarmService.setAlarmStatus(map);
+            jsonObject.put("code", ErrorCodeEnum.E00_0001.getCode());
+            jsonObject.put("message",ErrorCodeEnum.E00_0001.getMessage());
+            return jsonObject;
+        }catch (Exception e){
+            jsonObject.put("code", ErrorCodeEnum.E00_0002.getCode());
+            jsonObject.put("message", ErrorCodeEnum.E00_0002.getMessage());
+            return jsonObject;
+        }
+    }
+
+    @RequestMapping(value="/getAlarmDetail",method= RequestMethod.POST)
+    @ApiOperation("获取报警同批次上召信息")
+    public JSONObject getAlarmDetail(@RequestParam Map<String,Object> map){
+        log.info("==== 获取报警同批次上召信息 ====");
+        JSONObject jsonObject = new JSONObject();
+        try{
+            List<HashMap<String,Object>> signalList = alarmService.getSignalList(map);
+            jsonObject.put("data",signalList);
+            jsonObject.put("code", ErrorCodeEnum.E00_0001.getCode());
+            jsonObject.put("message",ErrorCodeEnum.E00_0001.getMessage());
+            return jsonObject;
+        }catch (Exception e){
+            jsonObject.put("code", ErrorCodeEnum.E00_0002.getCode());
+            jsonObject.put("message", ErrorCodeEnum.E00_0002.getMessage());
+            return jsonObject;
+        }
+    }
+}
---
+++ src/main/java/com/nari/dhsm/service/serviceImpl/AlarmServiceImpl.java
@@ -0,0 +1,44 @@
+package com.nari.dhsm.service.serviceImpl;
+
+import com.nari.dhsm.dao.AlarmMapper;
+import com.nari.dhsm.service.AlarmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+/**
+ * @author zhangheng
+ * @version 1.0 2020/7/13
+ */
+@Service
+public class AlarmServiceImpl implements AlarmService {
+
+    @Autowired
+    private AlarmMapper alarmMapper;
+
+    @Override
+    public List<HashMap<String, Object>> getAlarmList(String deviceId) {
+        List<HashMap<String,Object>> alarmList = new ArrayList<>();
+        alarmList = alarmMapper.getAlarmMap(deviceId);
+        return alarmList;
+    }
+
+    @Override
+    public int getAlarmNum(String deviceId) {
+        int alarmNum = alarmMapper.getAlarmNum(deviceId);
+        return alarmNum;
+    }
+
+    @Override
+    public void setAlarmStatus(Map<String,Object> map){
+        alarmMapper.setAlarmStatus(map);
+    }
+
+    @Override
+    public List<HashMap<String,Object>> getSignalList(Map<String,Object> map){
+        List<HashMap<String,Object>> signalList = new ArrayList<>();
+        signalList = alarmMapper.getSignalMap(map);
+        return signalList;
+    }
+}
