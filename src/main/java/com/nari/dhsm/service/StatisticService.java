d9f1b42bb6e726c7f255458f96a620d6f77a0f11
yinyx <302699832@qq.com>
2020-07-27 09:34:18 +0800

    添加硬件插件接口

---
+++ src/main/java/com/nari/dhsm/service/StatisticService.java
@@ -0,0 +1,36 @@
+package com.nari.dhsm.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author zhangheng
+ * @version 1.0 2020/7/21
+ */
+public interface StatisticService {
+
+    String getStationName(String stationId);
+
+    int getDeviceNum(String stationId);
+
+    int getOnlineDeviceNum(String stationId);
+
+    int getFaultNum(String stationId);
+
+    int getSoftFaultNum(String stationId);
+
+    int getHardFaultNum(String stationId);
+
+    List<Map<String, Object>> getDeviceList(String stationId);
+
+    String getDeviceNameById(String deviceId);
+
+    int getSoftNumById(String deviceId);
+
+    int getHardNumById(String deviceId);
+
+    int getModuleNum(Map<String,Object> paramap);
+
+    int getModuleHistoryNum(Map<String,Object> paramap);
+}
