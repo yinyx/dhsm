b5d8e2938a3d78481afe43a61412e1a544a84803
yinyx <302699832@qq.com>
2020-09-03 14:03:26 +0800

    添加webSocket配置文件

---
+++ src/main/java/com/nari/dhsm/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.nari.dhsm.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author yinyx
+ * @version 1.0 2020/8/27
+ */
+@Configuration
+public class WebSocketConfig {
+    /**
+     * 注入一个ServerEndpointExporter,该Bean会自动注册使用@ServerEndpoint注解申明的websocket endpoint
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
