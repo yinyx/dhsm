0f2af2040a2900807bca96a6b98e6dc96806f882
yinyx <302699832@qq.com>
2020-09-03 17:35:08 +0800

    添加查询结果排序

--- src/main/java/com/nari/dhsm/DhsmApplication.java
+++ src/main/java/com/nari/dhsm/DhsmApplication.java
@@ -2,8 +2,10 @@ package com.nari.dhsm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class DhsmApplication {
 
     public static void main(String[] args) {
